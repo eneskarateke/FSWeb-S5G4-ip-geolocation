bac78d2ffb42f814fa5c1a5988e4fc9989bf9458
eneskarateke <eneskarateke@gmail.com>
2023-04-20 16:52:38 +0300

    ülke bayrağı hariç tamamlandı

--- ip/index.js
+++ ip/index.js
@@ -1,5 +1,7 @@
 //axios import buraya gelecek
 
+import axios from 'axios';
+
 var benimIP;
 
 
@@ -31,11 +33,27 @@ async function ipAdresimiAl(){
 	ADIM 5'e gelene kadar fonksiyonunuzu test etmek için ip nizi URL'ye manuel olarak ekleyebilirsiniz.
 */
 
+benimIP = axios.get("https://apis.ergineer.com/ipgeoapi/176.232.59.149")
+	.then(data => {
+	console.log("data from server >", data);
+	const sayfaImport = document.querySelector(".cards")
+	sayfaImport.append(myFunc(data));
+	})
+	.catch(err => {
+		console.log("Oops there is an error > ", err);
+	})
+	.finally(() => {
+		console.log("HTTP request is completed!")
+	})
+
 /*
 	ADIM 2: Geri döndürülen verileri inceleyin, bu sizin ip bilgileriniz! Bileşen fonksiyonunuzu geliştirmek içindeki bu veri yapısını
 	iyice anlamanız gerekmektedir.
 	
 */
+
+
+
 /*
 	ADIM 3: Argümanı sadece 1 nesne kabül eden bir fonksiyon oluşturun.
     DOM metotlarını ve özelliklerini kullanarak, şunları gerçekleştirin:
@@ -53,11 +71,65 @@ async function ipAdresimiAl(){
 	</div>
     </div>
 */
+const myFunc = ({data}) =>{
+	console.log("data inside myFunc", data)
+	const cevap = data;
 
+	const cardDiv = document.createElement("div");
+	cardDiv.setAttribute("class","card");
+
+	const imgElement = document.createElement("img");
+	imgElement.setAttribute("src", cevap.ülkebayrağı);
+	cardDiv.append(imgElement);
+
+	const infoDiv = document.createElement("div");
+	infoDiv.setAttribute("class", "card-info");
+	cardDiv.append(infoDiv);
+
+	const h3Element = document.createElement("h3");
+	h3Element.setAttribute("class","ip");
+	h3Element.textContent = `Ip adresi: ${cevap.sorgu}`;
+	infoDiv.append(h3Element);
+
+	const ulkeP = document.createElement("p");
+	ulkeP.setAttribute("class","ulke");
+	ulkeP.textContent = `Ülke adı: ${cevap.ülke}, Ülke Kodu: (${cevap.ülkeKodu})`;
+	infoDiv.append(ulkeP);
+
+	const enlemP = document.createElement("p");
+	enlemP.textContent = `Enlem: ${cevap.enlem} Boylam: ${cevap.boylam}`;
+	infoDiv.append(enlemP);
+
+	const sehirP = document.createElement("p");
+	sehirP.textContent = `Şehir: ${cevap.şehir}`;
+	infoDiv.append(sehirP);
+
+	const saatP = document.createElement("p");
+	saatP.textContent = `Saat Dilimi: ${cevap.saatdilimi}`;
+	infoDiv.append(saatP);
+
+	const paraP = document.createElement("p");
+	paraP.textContent = `Para birimi: ${cevap.parabirimi}`;
+	infoDiv.append(paraP);
+
+	const ispP = document.createElement("p");
+	ispP.textContent = `ISP: ${cevap.isp}`;
+	infoDiv.append(ispP);
+
+
+return cardDiv;
+	
+
+
+
+}
 /*
 	ADIM 4: API'den alınan verileri kullanarak ADIM 3'te verilen yapıda bir kart oluşturun ve 
 	bu kartı DOM olarak .cards elementinin içine ekleyin. 
 */
+
+
+
 
 /*
 	ADIM 5: Manuel olarak eklediğiniz IP adresini dinamiğe dönüştürün. 
